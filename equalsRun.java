d177932c8565d7e8b69f36cf0066915f6df12b5c
Ryan <39725571+r-zmuda@users.noreply.github.com>
2022-03-01 14:23:38 -0800

    Add files via upload

---
+++ equalsRun.java
@@ -0,0 +1,15 @@
+package chapter14;
+
+import java.util.*;
+
+public class equalsRun {
+	public static void main(String[] args) {
+		Stack<Integer> stack1 = new Stack<Integer>();
+		Stack<Integer> stack2 = new Stack<Integer>();
+		equals(stack1, stack2);
+	}
+
+	public static boolean equals(Stack<Integer> n1, Stack<Integer> n2) {
+		return true;
+	}
+}

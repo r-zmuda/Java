01655ab4eb11ae734d9c2107ebff3028f6a4aad6
Ryan <39725571+r-zmuda@users.noreply.github.com>
2022-03-01 14:16:45 -0800

    Delete repeat.java

--- repeat.java
+++
@@ -1,5 +0,0 @@
-public static String repeat(String s, int n) throws IllegalArgumentException {
-	if(n > 0) {return s + repeat(s, n - 1);}
-	else if (n == 0) {return "";}
-	else {throw new IllegalArgumentException("n must be positive.");}
-}
